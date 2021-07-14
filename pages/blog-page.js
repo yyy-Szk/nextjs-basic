ac7bbe9428c4e93a38b436f52bb194c42a560516
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-07-14 09:30:37 +0900

    getStaticProps使ってみる

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,12 +1,25 @@
 import Layout from '../components/Layout'
+import fetch from 'node-fetch';
+import { getPostsData } from '../lib/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((resolve) => resolve.json())
+        .then((json) => console.log("非同期だから後で出る"))
+    console.log("こっちが先に出る")
     return (
         <Layout title="Blog">
-            <h1>blog page</h1>
+            { posts && posts.map((post, index) => <p key={index}>aaa</p>)}
         </Layout>
 
     )
 }
 
 export default Blog
+
+export async function getStaticProps() {
+    
+    const posts = await getPostsData()
+
+    return { props: { posts: posts } }
+}
