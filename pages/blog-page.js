1fce69995d1ccec065bdf152b00e190ba42bd11a
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-07-15 22:01:35 +0900

    綺麗にした

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,25 +1,31 @@
-import Layout from '../components/Layout'
-import fetch from 'node-fetch';
-import { getPostsData } from '../lib/posts'
+import Layout from "../components/Layout";
+import fetch from "node-fetch";
+import Post from "../components/Post";
+import { getPostsData } from "../lib/posts";
 
 const Blog = ({ posts }) => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((resolve) => resolve.json())
-        .then((json) => console.log("非同期だから後で出る"))
-    console.log("こっちが先に出る")
-    return (
-        <Layout title="Blog">
-            { posts && posts.map((post, index) => <p key={index}>aaa</p>)}
-        </Layout>
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //     .then((resolve) => resolve.json())
+  //     .then((json) => console.log("非同期だから後で出る"));
+  //   console.log("こっちが先に出る");
+  //   <Post key={1} post={{ id: 1, title: "test" }} />
+//   {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+//   {posts && posts.map((post) => <Post key={post.id} post={post} />)}
 
-    )
-}
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+          <Post key={posts[0].key} post={posts[0]}/>
+      </ul>
+    </Layout>
+  );
+};
 
-export default Blog
+export default Blog;
 
 export async function getStaticProps() {
-    
-    const posts = await getPostsData()
+  const posts = await getPostsData();
+//   console.log(posts);
 
-    return { props: { posts: posts } }
+  return { props: { posts: posts } };
 }
